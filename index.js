f02cfa45beffb64492f49ae0b786b7ee18ea2be2
Sergey Deshko <axilhik@gmail.com>
2020-07-08 14:13:03 +0300

    Add task 4
    add index.js with task 4

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 "use strict";
 
 /**
- *
- * @deprecated - square root of the sum of the cubes of a number
+ * @deprecated - square root of number
+ * @type {number} - st
  */
 
-const mass =[4, 2, 5, 19, 13, 0, 10];
-
-let sum = 0;
-
-for (let i=0; i< mass.length; i++){
-         sum += Math.pow( Number(mass[i]) , 3);
-
-}
-console.log(Math.sqrt(sum));
-
+let st = Math.sqrt(245);
+console.log(st)
