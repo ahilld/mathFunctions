2fc76557dc473c2546fc7fe5fbc3bc204634b957
Sergey Deshko <axilhik@gmail.com>
2020-07-08 12:47:39 +0300

    Add task 3
    add index.js with task 3

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 "use strict";
 
-balance(12, 3);
-balance(11, 3);
-
 /**
- *
- * @param {number} a
- * @param {number} b
+ * @deprecated - raising 2 to 10 degree
+ * @type {number} - st
  */
 
-function balance(a, b,) {
-    if ( a % b === 0) {
-        console.log (`Делится; a / b = ${a / b}`);
-    } else {
-        console.log (`Делится с остатком; остаток: ${a % b} `);
-    }
-}
+let st = Math.pow(2, 10);
+console.log(st)
+
