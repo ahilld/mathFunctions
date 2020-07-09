a3e80aebf71caca6d047b7b8248e0a6f18fb7d6f
Sergey Deshko <axilhik@gmail.com>
2020-07-09 13:06:14 +0300

    Add task 8
    add index.js with task 8

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ let obj = {
     floor: Math.floor(st),
     ceil: Math.ceil(st),
 }
+
+/**
+ * Даны числа 4, -2, 5, 19, -130, 0, 10.
+ * Найдите минимальное и максимальное число.
+ */
+
+let maxNum = Math.max(4, -2, 5, 19, -130, 0, 10);
+
+let minNum = Math.min(4, -2, 5, 19, -130, 0, 10);
+
+console.log(maxNum);
+console.log(minNum);
