32532fc88282cb9f190d0db156025bd71275faf5
Sergey Deshko <axilhik@gmail.com>
2020-07-09 13:54:26 +0300

    Add task 9
    add index.js with task 9

--- index.js
+++ index.js
@@ -24,3 +24,18 @@ let minNum = Math.min(4, -2, 5, 19, -130, 0, 10);
 
 console.log(maxNum);
 console.log(minNum);
+
+/**
+ * @deprecated - Вывoдит на экран случайное целое число от 1 до 100.
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+
+getRandomInt(1,100);
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
